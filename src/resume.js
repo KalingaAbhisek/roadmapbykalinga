fd63f6ada3cc94f61deffd20f94e1b66f5d19745
KALINGA ABHISEK <kalingaabhisek@outlook.com>
2022-10-10 21:32:01 +0530

    check https://kalinga.tech

--- src/resume.js
+++ src/resume.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const resume=()=>{
     return(
-        <iframe src="https://docs.google.com/document/d/19Tp1ZNW5yXvLmEaXG40vCIWFC0nsleEY6qIv49rc3Z8/edit?usp=sharing" style={{height: '100vh', width: '100vw'}}/>
+        <iframe title="pdf" src="https://docs.google.com/document/d/19Tp1ZNW5yXvLmEaXG40vCIWFC0nsleEY6qIv49rc3Z8/edit?usp=sharing" style={{height: '100vh', width: '100vw'}}/>
     )
 }
 export default resume;
